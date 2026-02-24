c36d13c24efaa38b8c3d8c193f6418e2246b03f5
İbo <icaglary@gmail.com>
2026-02-25 02:56:37 +0300

    Add receiver ID and available links to config

--- data/receiver-info-config.php
+++ data/receiver-info-config.php
@@ -12,6 +12,14 @@ const RECEIVER_INFO = `
         <li>• 131.725 MHz - VDL M0/A - ACARS - EUR - SITA Aircom</li>
         <li>• 131.825 MHz - VDL M0/A - ACARS - EUR - ARINC GlobalLink</li>
     </ul>
+    <h3>Receiver ID: ANKARA1</h3>
+    <h4>Available Links:</h4>
+    <p>(Frequency - Subnetwork Designator - Network Type - Region - Datalink Service Provider)</p>
+    <ul class="frequency-list">
+        <li>• 131.525 MHz - VDL M0/A - ACARS - EUR - </li>
+        <li>• 131.725 MHz - VDL M0/A - ACARS - EUR - SITA Aircom</li>
+        <li>• 131.825 MHz - VDL M0/A - ACARS - EUR - ARINC GlobalLink</li>
+    </ul>
 </div>
 `;
 </script>
